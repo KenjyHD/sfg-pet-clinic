f72086643092ea782f911d4e3aca641bb2325a93
KenjyHD <liviu.calmis@mail.ru>
2023-05-31 14:14:53 +0300

    added vet list, updated controller

--- pet-clinic-web/src/main/java/kenjy/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/kenjy/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package kenjy.sfgpetclinic.controllers;
 
+import kenjy.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listOwners() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listOwners(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
