f03e3ef95d2a304fa019763e5cd8438d1f83a688
KenjyHD <liviu.calmis@mail.ru>
2023-06-06 12:48:23 +0300

    fixed broken links

--- pet-clinic-web/src/main/java/kenjy/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/kenjy/sfgpetclinic/controllers/OwnerController.java
@@ -22,4 +22,9 @@ public class OwnerController {
 
         return "owners/index";
     }
+
+    @RequestMapping({"/find", "/oups"})
+    public String findOwners() {
+        return "notimplemented";
+    }
 }
