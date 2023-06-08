a43ad1018d64784da22e2785799618dfc80d305f
KenjyHD <liviu.calmis@mail.ru>
2023-06-08 13:06:47 +0300

    updated Vet to use JPA

--- pet-clinic-data/src/main/java/kenjy/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/kenjy/sfgpetclinic/model/Specialty.java
@@ -1,6 +1,11 @@
 package kenjy.sfgpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
 public class Specialty extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
