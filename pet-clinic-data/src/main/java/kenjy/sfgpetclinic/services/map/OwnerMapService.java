d43aa3ff101ed1e57be2e36e96ff007f5e4402f5
KenjyHD <liviu.calmis@mail.ru>
2023-06-19 12:52:02 +0300

    added find owner function and tests

--- pet-clinic-data/src/main/java/kenjy/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/kenjy/sfgpetclinic/services/map/OwnerMapService.java
@@ -5,10 +5,10 @@ import kenjy.sfgpetclinic.model.Pet;
 import kenjy.sfgpetclinic.services.OwnerService;
 import kenjy.sfgpetclinic.services.PetService;
 import kenjy.sfgpetclinic.services.PetTypeService;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -77,6 +77,11 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
                 .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        return null;
     }
 
 //    public Owner findByLastNameL(String lastName) {
