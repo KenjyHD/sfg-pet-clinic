52f3e8508e7e21541aa732d599bc858b6c2683cb
KenjyHD <liviu.calmis@mail.ru>
2023-06-19 18:29:00 +0300

    added model attributes for Pet Controller

--- pet-clinic-web/src/main/java/kenjy/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/kenjy/sfgpetclinic/controllers/PetController.java
@@ -1,49 +1,52 @@
-//package kenjy.sfgpetclinic.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.ui.ModelMap;
-//import org.springframework.util.StringUtils;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.WebDataBinder;
-//import org.springframework.web.bind.annotation.*;
-//
-//import jakarta.validation.Valid;
-//import java.util.Collection;
-//
-///**
-// * Created by jt on 9/22/18.
-// */
-//@Controller
-//@RequestMapping("/owners/{ownerId}")
-//public class PetController {
-//
-//    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
-//
-//    private final PetService petService;
-//    private final OwnerService ownerService;
-//    private final PetTypeService petTypeService;
-//
-//    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
-//        this.petService = petService;
-//        this.ownerService = ownerService;
-//        this.petTypeService = petTypeService;
-//    }
-//
-//    @ModelAttribute("types")
-//    public Collection<PetType> populatePetTypes() {
-//        return petTypeService.findAll();
-//    }
-//
-//    @ModelAttribute("owner")
-//    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
-//        return ownerService.findById(ownerId);
-//    }
-//
-//    @InitBinder("owner")
-//    public void initOwnerBinder(WebDataBinder dataBinder) {
-//        dataBinder.setDisallowedFields("id");
-//    }
+package kenjy.sfgpetclinic.controllers;
+
+import kenjy.sfgpetclinic.model.Owner;
+import kenjy.sfgpetclinic.model.Pet;
+import kenjy.sfgpetclinic.model.PetType;
+import kenjy.sfgpetclinic.services.OwnerService;
+import kenjy.sfgpetclinic.services.PetService;
+import kenjy.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
 //
 //    @GetMapping("/pets/new")
 //    public String initCreationForm(Owner owner, Model model) {
@@ -88,5 +91,5 @@
 //            return "redirect:/owners/" + owner.getId();
 //        }
 //    }
-//
-//}
+
+}
